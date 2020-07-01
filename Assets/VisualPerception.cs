4f09e6810874915e9976171f5be2f2b42e0ee1bf
Patsomir <pnn246@gmail.com>
2020-07-01 13:14:38 +0300

    Implement visual perception
    Added a class that determines whether the player is in an enemy's
    line of sight.

---
+++ Assets/VisualPerception.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+
+public class VisualPerception : MonoBehaviour
+{
+    Transform player = null;
+    int playerLayer = 8;
+    int groundLayer = 9;
+
+    [SerializeField]
+    private float sightDistance = 10;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public bool CanSeePlayer()
+    {
+        if ((player.position.x - transform.root.position.x) * transform.root.localScale.x < 0)
+        {
+            return false;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,
+                                             player.position - transform.position,
+                                             sightDistance, 1 << playerLayer | 1 << groundLayer);
+        if (hit.collider != null)
+        {
+            if (hit.collider.transform.root.CompareTag("Player"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
