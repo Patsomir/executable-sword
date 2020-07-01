8e80c1cac7c391be1db7beeca0519fc23d7b06e6
Patsomir <pnn246@gmail.com>
2020-07-01 20:09:38 +0300

    Implement Ripped Guy death
    Ripped guy dies when he is killed.

---
+++ Assets/AIRippedGuyDeadState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AIRippedGuyDeadState : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<Rigidbody2D>().freezeRotation = false;
+        animator.transform.Translate(1.5f * Vector2.up, Space.Self);
+    }
+}
---
+++ Assets/RippedGuyAnimatorManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RippedGuyAnimatorManager : MonoBehaviour
+{
+    Animator animator = null;
+    HealthManager health = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Awake()
+    {
+        health = GetComponent<HealthManager>();
+    }
+
+    void SetDeathTrigger()
+    {
+        animator.SetTrigger("ShouldDie");
+    }
+
+    private void OnEnable()
+    {
+        health.OnDeath += SetDeathTrigger;
+    }
+
+    private void OnDisable()
+    {
+        health.OnDeath -= SetDeathTrigger;
+    }
+}
