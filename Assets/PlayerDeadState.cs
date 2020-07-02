771f0fed1cfe88bef62813661fa01d7e7f044865
Patsomir <pnn246@gmail.com>
2020-07-02 15:30:27 +0300

    Implement player death
    Added a new player sprite. The player can no longer move or shoot
    when they run out of HP.

---
+++ Assets/PlayerDeadState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeadState : StateMachineBehaviour
+{
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Rigidbody2D body = animator.GetComponent<Rigidbody2D>();
+        body.freezeRotation = false;
+        body.sharedMaterial = null;
+    }
+}
---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerAnimatorManager : MonoBehaviour
+{
+    private Animator animator = null;
+    private Rigidbody2D body = null;
+    private MovementController controller = null;
+    private HealthManager health = null;
+
+    [SerializeField]
+    private float fallingThreshold = 0.1f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        controller = GetComponent<MovementController>();
+        health = GetComponent<HealthManager>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("NormalizedSpeed", Mathf.Abs(body.velocity.x) / controller.MaxRunningSpeed);
+        animator.SetBool("IsFalling", body.velocity.y < fallingThreshold);
+    }
+
+    void TriggerJumpAnimation()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    void TriggerDeathAnimation()
+    {
+        animator.SetTrigger("ShouldDie");
+    }
+
+    private void OnEnable()
+    {
+        controller.OnJumpStart += TriggerJumpAnimation;
+        health.OnDeath += TriggerDeathAnimation;
+    }
+
+    private void OnDisable()
+    {
+        controller.OnJumpStart -= TriggerJumpAnimation;
+        health.OnDeath -= TriggerDeathAnimation;
+    }
+}
