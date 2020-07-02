248671d6ccd0b74dc867bc3f3b25a068b80428ff
Patsomir <pnn246@gmail.com>
2020-07-02 23:48:47 +0300

    Add evaporation to player
    The player evaporates when they run out of HP. The shooter also
    disappears.

---
+++ Assets/Scripts/ShooterAnimatorManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ShooterAnimatorManager : MonoBehaviour
+{
+    private Animator animator = null;
+    private Shooter shooter = null;
+    private HealthManager health = null;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthManager>();
+    }
+
+    public void TriggerShoot()
+    {
+        animator.SetTrigger("Shoot");
+    }
+
+    public void TriggerDisappear()
+    {
+        animator.SetTrigger("Disappear");
+    }
+
+    private void OnEnable()
+    {
+        shooter.OnShoot += TriggerShoot;
+        if(health != null)
+        {
+            health.OnDeath += TriggerDisappear;
+        }
+    }
+
+    private void OnDisable()
+    {
+        shooter.OnShoot -= TriggerShoot;
+        if (health != null)
+        {
+            health.OnDeath -= TriggerDisappear;
+        }
+    }
+}
