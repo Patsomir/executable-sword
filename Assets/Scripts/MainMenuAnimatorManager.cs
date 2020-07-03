e8d69a4ed11bd50fd86caa71886d432eca7013e8
Patsomir <pnn246@gmail.com>
2020-07-03 20:07:29 +0300

    Implement Main Menu functionalities
    Implemented the various buttons on the main menu.

---
+++ Assets/Scripts/MainMenuAnimatorManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MainMenuAnimatorManager : MonoBehaviour
+{
+    private Animator animator = null;
+    private MainMenuManager manager = null;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Awake()
+    {
+        manager = GetComponent<MainMenuManager>();
+    }
+
+    private void SetTransition()
+    {
+        animator.SetTrigger("Transition");
+    }
+
+    private void OnEnable()
+    {
+        manager.OnTransitionStart += SetTransition;
+    }
+
+    private void OnDisable()
+    {
+        manager.OnTransitionStart -= SetTransition;
+    }
+}
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,65 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField]
-    private Button playButton = null;
-    void Start()
+    private GameObject controlsMenu = null;
+
+    [SerializeField]
+    private GameObject creditsMenu = null;
+
+    private GameObject currentMenu = null;
+
+    public Action OnTransitionStart;
+
+    public void Play()
     {
-        playButton.onClick.AddListener(Play);
+        StartCoroutine(StartGameCoroutine(0.5f));
     }
 
-    void Play()
+    public void Controls()
     {
+        StartCoroutine(TransitionCoroutine(controlsMenu, 0.5f));
+    }
+
+    public void Credits()
+    {
+        StartCoroutine(TransitionCoroutine(creditsMenu, 0.5f));
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Return()
+    {
+        StartCoroutine(ReturnCoroutine(0.5f));
+    }
+
+    private IEnumerator TransitionCoroutine(GameObject menu, float delay)
+    {
+        OnTransitionStart?.Invoke();
+        currentMenu = menu;
+        yield return new WaitForSeconds(delay/2);
+        menu.SetActive(true);
+    }
+
+    private IEnumerator StartGameCoroutine(float delay)
+    {
+        OnTransitionStart?.Invoke();
+        yield return new WaitForSeconds(delay / 2);
         SceneManager.LoadScene("TileSampleScene");
+    }
+
+    private IEnumerator ReturnCoroutine(float delay)
+    {
+        OnTransitionStart?.Invoke();
+        yield return new WaitForSeconds(delay/2);
+        currentMenu.SetActive(false);
+        currentMenu = null;
     }
 }
